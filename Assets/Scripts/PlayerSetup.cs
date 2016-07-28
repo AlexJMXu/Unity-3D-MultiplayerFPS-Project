666ae59b58227b103ef907726f5d39c83a40a62b
iAlex95 <ajm.xu@outlook.com>
2016-07-28 01:20:09 +0100

    Implemented Feature - Shooting

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -5,18 +5,38 @@ public class PlayerSetup : NetworkBehaviour {
 
 	[SerializeField] private Behaviour[] componentsToDisable;
 
+	[SerializeField] private string remoteLayerName = "RemotePlayer";
+
+	[SerializeField] private string playerPrefix = "Player ";
+
 	public Camera sceneCamera;
 
 	void Start() {
 		if (!isLocalPlayer) {
-			for (int i = 0; i < componentsToDisable.Length; i++) {
-				componentsToDisable[i].enabled = false;
-			}
+			DisableComponents();
+			AssignRemoteLayer();
 		} else {
 			sceneCamera = Camera.main;
 			if (sceneCamera != null) {
 				sceneCamera.gameObject.SetActive(false);
 			}
+		}
+
+		RegisterPlayer();
+	}
+
+	void RegisterPlayer() {
+		string _ID = playerPrefix + GetComponent<NetworkIdentity>().netId;
+		transform.name = _ID;
+	}
+
+	void AssignRemoteLayer() {
+		gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+	}
+
+	void DisableComponents() {
+		for (int i = 0; i < componentsToDisable.Length; i++) {
+			componentsToDisable[i].enabled = false;
 		}
 	}
 
