e1e1c0b9f3d1e20fa1fccb726f513df661478dbb
iAlex95 <ajm.xu@outlook.com>
2016-07-28 18:41:31 +0100

    Implemented - Player Model & Animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(ConfigurableJoint))]
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
@@ -13,24 +14,29 @@ public class PlayerController : MonoBehaviour {
 	[SerializeField] private float jointSpring = 20f;
 	[SerializeField] private float jointMaxForce = 40f;
 
+	// Component Cache
 	private PlayerMotor motor;
 	private ConfigurableJoint joint;
+	private Animator animator;
 
 	void Start() {
 		motor = GetComponent<PlayerMotor>();
 		joint = GetComponent<ConfigurableJoint>();
+		animator = GetComponent<Animator>();
 
 		SetJointSettings(jointSpring);
 	}
 
 	void Update() {
 		// Calculate movement velocity as a 3D Vector
-		float _xMove = Input.GetAxisRaw("Horizontal");
-		float _zMove = Input.GetAxisRaw("Vertical");
+		float _xMove = Input.GetAxis("Horizontal");
+		float _zMove = Input.GetAxis("Vertical");
 		Vector3 _movHorizontal = transform.right * _xMove; // (1, 0, 0) Vector
 		Vector3 _movVertical = transform.forward * _zMove; // (0, 0, 1) Vector
 		// Final movement vector
-		Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+		Vector3 _velocity = (_movHorizontal + _movVertical) * speed;
+		// Animate movement
+		animator.SetFloat("ForwardVelocity", _zMove);
 		// Apply movement
 		motor.Move(_velocity);
 
